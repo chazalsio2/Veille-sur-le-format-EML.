4fc5c7f33ae57097243866ed3ce869952a5044df
chazalsio2 <chazal.hassani@gmail.com>
2021-12-17 14:43:54 +0400

    Ajoutez des fichiers projet.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sujet = new System.Windows.Forms.TextBox();
+            this.mailde = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.openfile = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "De:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sujet:";
+            // 
+            // mail
+            // 
+            this.mail.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.mail.BackColor = System.Drawing.Color.SeaShell;
+            this.mail.Location = new System.Drawing.Point(178, 116);
+            this.mail.Name = "mail";
+            this.mail.Size = new System.Drawing.Size(604, 22);
+            this.mail.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Message:";
+            // 
+            // sujet
+            // 
+            this.sujet.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.sujet.BackColor = System.Drawing.Color.SeaShell;
+            this.sujet.Location = new System.Drawing.Point(178, 197);
+            this.sujet.Name = "sujet";
+            this.sujet.Size = new System.Drawing.Size(604, 22);
+            this.sujet.TabIndex = 5;
+            this.sujet.TextChanged += new System.EventHandler(this.sujet_TextChanged);
+            // 
+            // mailde
+            // 
+            this.mailde.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.mailde.BackColor = System.Drawing.Color.SeaShell;
+            this.mailde.Location = new System.Drawing.Point(178, 156);
+            this.mailde.Name = "mailde";
+            this.mailde.Size = new System.Drawing.Size(604, 22);
+            this.mailde.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "A:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox1.BackColor = System.Drawing.Color.SeaShell;
+            this.textBox1.Location = new System.Drawing.Point(133, 270);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(649, 361);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox2.BackColor = System.Drawing.Color.SeaShell;
+            this.textBox2.Location = new System.Drawing.Point(133, 650);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(649, 164);
+            this.textBox2.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 646);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Piece joint:";
+            // 
+            // openfile
+            // 
+            this.openfile.Location = new System.Drawing.Point(21, 61);
+            this.openfile.Name = "openfile";
+            this.openfile.Size = new System.Drawing.Size(140, 23);
+            this.openfile.TabIndex = 13;
+            this.openfile.Text = "Selectionner un Mail";
+            this.openfile.UseVisualStyleBackColor = true;
+            this.openfile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox3.BackColor = System.Drawing.Color.SeaShell;
+            this.textBox3.Location = new System.Drawing.Point(178, 62);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(604, 22);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(874, 978);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.openfile);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.mailde);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.sujet);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.mail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox mail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox sujet;
+        private System.Windows.Forms.TextBox mailde;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button openfile;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void sujet_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            //label4.MaximumSize = 30;
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "il n'y a pas de piece joint";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Text files (*.eml)|*.eml|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+           
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var fileInfo = new FileInfo(openFileDialog.FileName);
+                var eml = MsgReader.Mime.Message.Load(fileInfo);
+                textBox3.Text = fileInfo.Name;
+
+
+
+                if (eml.Headers != null)
+                {
+
+                    if (eml.Headers.To != null)
+                    {
+                        foreach (var recipient in eml.Headers.To)
+                        {
+                            var to = recipient.Address;
+                            mailde.Text = to;
+                        }
+                    }
+                    if (eml.Headers.From != null)
+                    {
+                        var from = eml.Headers.From;
+                        mail.Text = from.Address;
+
+                    }
+
+
+                }
+                if (eml.Attachments != null)
+                {
+                    var Attachment = eml.Attachments.ToArray();
+                   
+                    foreach (var attachment in Attachment)
+                    {
+                        var piece_j = "FileName : " + attachment.FileName + " \r\n";
+                        piece_j += "ContentId : " + attachment.ContentId + " \r\n";
+                        piece_j += "ContentDescription : " + attachment.ContentDescription + " \r\n";
+                        piece_j += "ContentDisposition : " + attachment.ContentDisposition + " \r\n";
+                        piece_j += "ContentTransferEncoding : " + attachment.ContentTransferEncoding + " \r\n";
+                        piece_j += "ContentType : " + attachment.ContentType + " \r\n";
+                        piece_j += "Body : " + attachment.Body + " \r\n";
+                        piece_j += "BodyEncoding : " + attachment.BodyEncoding + " \r\n";
+                        piece_j += "IsAttachment : " + attachment.IsAttachment + " \r\n";
+                        piece_j += "IsInline : " + attachment.IsInline + " \r\n";
+                        piece_j += "IsMultiPart : " + attachment.IsMultiPart + " \r\n";
+                        piece_j += "IsText : " + attachment.IsText + " \r\n";
+                        piece_j += "MessageParts : " + attachment.MessageParts + " \r\n";
+                        piece_j += "\r\n";
+
+                        textBox2.Text += piece_j;
+
+                    }
+
+                }
+                else
+                {
+                    textBox2.Text += "hello";
+                }
+
+                var subject = eml.Headers.Subject;
+
+                if (subject != null)
+                {
+                    sujet.Text = subject;
+                }
+                if (eml.TextBody != null)
+                {
+                    var textBody = System.Text.Encoding.UTF8.GetString(eml.TextBody.Body);
+                    textBox1.Text = textBody;
+                    //var p = System.Text.Encoding.UTF8.GetString(eml.Headers.To);
+
+                }
+                else
+                {
+                    textBox1.Text = "il n'y a pas de message";
+                }
+
+                if (eml.HtmlBody != null)
+                {
+                    var htmlBody = System.Text.Encoding.UTF8.GetString(eml.HtmlBody.Body);
+
+                }
+
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
